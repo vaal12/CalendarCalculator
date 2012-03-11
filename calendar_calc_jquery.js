5d66919a7f2a6d6c05875c976998c2917e653387
Alexey Vassiliev <vaal12@yandex.ru>
2012-03-11 18:43:55 +0200

    Good design. Removed ng library.

--- calendar_calc_jquery.js
+++ calendar_calc_jquery.js
@@ -5,31 +5,31 @@ startDatesHistory = new Array();
 
 
 $(document).ready(function(){
-	console.log("Hello console");
+	//console.log("Hello //console");
 	//$("#start_cal_holder_div").text("Hello I am calendar");
 	initEverything();
 	
 });//$(document).ready(function(){
 
-function onStartDateHistoryClick(eventObj){
-	console.log("Start history date clicked:"+eventObj.target);
-	console.log("Start history date clicked:"+eventObj.currentTarget.id);
+function onStartDateHistoryClick(eventObj) {
+	//console.log("Start history date clicked:"+eventObj.target);
+	//console.log("Start history date clicked:"+eventObj.currentTarget.id);
 	
 	dateStr = eventObj.currentTarget.id.replace("_", "/");
 	dateStr = dateStr.replace("_", "/");
-	console.log("We have string:"+dateStr);
+	//console.log("We have string:"+dateStr);
 	//dateObj = new Date(dateStr);
-	//console.log(dateObj.toLocaleString());
+	////console.log(dateObj.toLocaleString());
 	$('#start_cal_holder_div').datepicker("setDate", dateStr);
 	
 	$('#dialog').dialog('close');
 }
 
 function startDateSelected(dateStr, instance){
-	console.log("startDateSelected:"+dateStr);
+	//console.log("startDateSelected:"+dateStr);
 	startDatesHistory.push(dateStr);
 	dateObj = new Date(dateStr);
-	console.log("We have date object:"+dateObj.toLocaleString());
+	//console.log("We have date object:"+dateObj.toLocaleString());
 	idString = (dateObj.getMonth()+1)+"_"+dateObj.getDate()+"_"+dateObj.getFullYear();
 	newLinkStr = "<a href='#' id='"+idString+"'>"+dateObj.toLocaleString()+"</a>";
 	$("#startDateHistoryContent").html(
@@ -38,7 +38,7 @@ function startDateSelected(dateStr, instance){
 	i=0;
 	while(i<startDatesHistory.length){
 		dateStr = startDatesHistory[i];
-		console.log("Setting event for date:"+dateStr);
+		//console.log("Setting event for date:"+dateStr);
 		dateObj = new Date(dateStr);
 		idString = (dateObj.getMonth()+1)+"_"+dateObj.getDate()+"_"+dateObj.getFullYear();
 		$("#"+idString).click(onStartDateHistoryClick);
@@ -48,7 +48,7 @@ function startDateSelected(dateStr, instance){
 };
 
 function initEverything(){
-	console.log("initEverything starts");
+	//console.log("initEverything starts");
 	//$("#start_cal_holder_div").hide();
 	// Datepicker
 	$('#start_cal_holder_div').datepicker({
@@ -67,15 +67,15 @@ function initEverything(){
 	});//$('#end_cal_holder_div').datepicker({
 	
 	$("#start_year_back").click(function(){
-		console.log("Year back pressed");
-		console.log("Datepicker date:"+
-				$('#start_cal_holder_div').datepicker("getDate")
-				);
+		//console.log("Year back pressed");
+		//console.log("Datepicker date:"+
+				//$('#start_cal_holder_div').datepicker("getDate")
+				//);
 		$('#start_cal_holder_div').datepicker("setDate", "02/14/1978");
 	});//$("#start_year_back").click(function(){
 	
 	$("#end_cal_holder_div").click(function(){
-		console.log("End cal holder click");
+		//console.log("End cal holder click");
 		//$("#start_cal_holder_div").show();
 		//$("#start_cal_holder_div").addClass("color='red'");
 	});//#("#end_cal_holder_div").click(function(){
@@ -89,10 +89,9 @@ function initEverything(){
 	
 	// Dialog Link
 	$('#dialog_link').click(function(eventObj){
-		console.log("We have mouse click. X="+
-				eventObj.clientX+
-				"Y="+eventObj.clientY
-			);
+		//console.log("We have mouse click. X="+
+				//eventObj.clientX+
+				//"Y="+eventObj.clientY 	);
 		$('#dialog').dialog( "option", "position", [eventObj.clientX, eventObj.clientY-30] );
 		$('#dialog').dialog('open');
 		return false;
