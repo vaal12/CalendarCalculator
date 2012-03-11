23d9e41df073eec365c37cafefcfe8792494e13e
Alexey Vassiliev <vaal12@yandex.ru>
2012-03-11 17:14:19 +0200

    Working history dialog

--- calendar_calc_jquery.js
+++ calendar_calc_jquery.js
@@ -1,11 +1,51 @@
 
 
+startDatesHistory = new Array();
+
+
+
 $(document).ready(function(){
 	console.log("Hello console");
 	//$("#start_cal_holder_div").text("Hello I am calendar");
 	initEverything();
 	
 });//$(document).ready(function(){
+
+function onStartDateHistoryClick(eventObj){
+	console.log("Start history date clicked:"+eventObj.target);
+	console.log("Start history date clicked:"+eventObj.currentTarget.id);
+	
+	dateStr = eventObj.currentTarget.id.replace("_", "/");
+	dateStr = dateStr.replace("_", "/");
+	console.log("We have string:"+dateStr);
+	//dateObj = new Date(dateStr);
+	//console.log(dateObj.toLocaleString());
+	$('#start_cal_holder_div').datepicker("setDate", dateStr);
+	
+	$('#dialog').dialog('close');
+}
+
+function startDateSelected(dateStr, instance){
+	console.log("startDateSelected:"+dateStr);
+	startDatesHistory.push(dateStr);
+	dateObj = new Date(dateStr);
+	console.log("We have date object:"+dateObj.toLocaleString());
+	idString = (dateObj.getMonth()+1)+"_"+dateObj.getDate()+"_"+dateObj.getFullYear();
+	newLinkStr = "<a href='#' id='"+idString+"'>"+dateObj.toLocaleString()+"</a>";
+	$("#startDateHistoryContent").html(
+			$("#startDateHistoryContent").html()+"<br>"+newLinkStr
+			);
+	i=0;
+	while(i<startDatesHistory.length){
+		dateStr = startDatesHistory[i];
+		console.log("Setting event for date:"+dateStr);
+		dateObj = new Date(dateStr);
+		idString = (dateObj.getMonth()+1)+"_"+dateObj.getDate()+"_"+dateObj.getFullYear();
+		$("#"+idString).click(onStartDateHistoryClick);
+		i+=1;
+	};
+	
+};
 
 function initEverything(){
 	console.log("initEverything starts");
@@ -17,7 +57,8 @@ function initEverything(){
 		changeYear : true,
 		changeMonth : true,
 		yearRange: '1971:2020',
-		showButtonPanel: true
+		showButtonPanel: true,
+		onSelect: startDateSelected
 	});//$('#start_cal_holder_div').datepicker({
 	
 	$('#end_cal_holder_div').datepicker({
@@ -38,6 +79,25 @@ function initEverything(){
 		//$("#start_cal_holder_div").show();
 		//$("#start_cal_holder_div").addClass("color='red'");
 	});//#("#end_cal_holder_div").click(function(){
+	
+	// Dialog			
+	$('#dialog').dialog({
+		autoOpen: false,
+		title : "Start date history",
+		width : 300 
+	});//$('#dialog').dialog({
+	
+	// Dialog Link
+	$('#dialog_link').click(function(eventObj){
+		console.log("We have mouse click. X="+
+				eventObj.clientX+
+				"Y="+eventObj.clientY
+			);
+		$('#dialog').dialog( "option", "position", [eventObj.clientX, eventObj.clientY-30] );
+		$('#dialog').dialog('open');
+		return false;
+	});
+	
 };//function initEverythin(){
 
 
