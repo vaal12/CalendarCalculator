125bca8b3975e0d0f2e86330b567abb113c9cd03
Alexey Vassiliev <vaal12@yandex.ru>
2012-03-13 15:30:08 +0200

    Initial release. Put to vassiliev.com.ua calendar

--- calendar_calc_jquery.js
+++ calendar_calc_jquery.js
@@ -121,6 +121,8 @@ function initEverything(){
 	
 	
 };//function initEverythin(){
+	
+	
 
 
 function removeStartCalendarRow(){
--- endlessCalendar.js
+++ endlessCalendar.js
@@ -14,5 +14,42 @@ $(document).ready(function(){
 		yearRange: '1971:2020',
 		showButtonPanel: true
 	});//$('#start_cal_holder_div').datepicker({
+		
+	$("#addRow").click(addRow);
+	$("#removeRow").click(removeRow);
+	
+	$("#addCol").click(addCol);
+	$("#removeCol").click(removeCol);
 	
 });//$(document).ready(function(){
+
+function changeNumberOfRowsCols(){
+	$('#calendarPlace').datepicker("option",
+			"numberOfMonths", [startCalendarNumberOfRows, startCalendarNumberOfCols]);
+	//$('#start_cal_holder_div').datepicker("setDate", startDateCurrent);
+}
+
+	
+function removeRow(){
+	if(startCalendarNumberOfRows>1) {
+		startCalendarNumberOfRows -=1;
+		changeNumberOfRowsCols();
+	}
+}
+
+function addRow(){
+	startCalendarNumberOfRows +=1;
+	changeNumberOfRowsCols();
+}
+
+function removeCol(){
+	if(startCalendarNumberOfCols>1){
+		startCalendarNumberOfCols -=1;
+		changeNumberOfRowsCols();
+	}
+}
+
+function addCol(){
+	startCalendarNumberOfCols +=1;
+	changeNumberOfRowsCols();
+}
