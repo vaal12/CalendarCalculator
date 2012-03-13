c582149e09a5989b5f087f77ef58f1b6088086c4
Alexey Vassiliev <vaal12@yandex.ru>
2012-03-13 19:33:23 +0200

    Issue with summer/winter time leads to recalculation of the time offset for gettime function (that is it cannot be used for calculation of day span as in March and October it is off by 1 hour. DATE UTC can be used for that.

--- endlessCalendar.js
+++ endlessCalendar.js
@@ -1,6 +1,9 @@
 startCalendarNumberOfRows = 1;
 startCalendarNumberOfCols = 3;
 
+startDate = null;
+endDate = null;
+
 $(document).ready(function(){
 	//console.log("Hello //console");
 	//$("#start_cal_holder_div").text("Hello I am calendar");
@@ -12,8 +15,10 @@ $(document).ready(function(){
 		changeYear : true,
 		changeMonth : true,
 		yearRange: '1971:2020',
-		showButtonPanel: true
+		showButtonPanel: true,
+		onSelect: dateSelected
 	});//$('#start_cal_holder_div').datepicker({
+	
 		
 	$("#addRow").click(addRow);
 	$("#removeRow").click(removeRow);
@@ -21,7 +26,57 @@ $(document).ready(function(){
 	$("#addCol").click(addCol);
 	$("#removeCol").click(removeCol);
 	
+	i=2;
+	millisInDay = 86400000;
+	year = "2004";
+	mar1Date = new Date("03/01/"+year);
+	$("#daysMillis").html("Start date:"+mar1Date.toLocaleString()+"<br>");
+	while(i<32) {
+		marOtherDate = new Date("03/"+i+"/"+year);
+		millisCalculated = ((i-1)*millisInDay);
+		addStr = $("#daysMillis").html() + i+":"+millisCalculated;
+		millisDate = marOtherDate.getTime()-mar1Date.getTime();
+		addStr = addStr + "  ________"+millisDate+":(calculated)";
+		addStr = addStr + "_________Difference:"+(millisCalculated - millisDate)+"<br>";
+		$("#daysMillis").html(addStr);
+		i+=1;
+	}
+	
 });//$(document).ready(function(){
+
+function log2Console(logStr){
+	console.log(logStr);
+}
+
+function dateSelected(dateStr, instance){
+	log2Console(dateStr);
+	selectedDate = new Date(dateStr+" 00:01 PM");
+	log2Console("Date parsed:"+selectedDate.toDateString()); 
+	if(startDate==null){//This is start date
+		$("#startDateText").text("Start date: "+selectedDate.toDateString());
+		startDate = selectedDate;
+		return
+	}
+	if(selectedDate < startDate){
+		log2Console("Selected date is < startDate");
+		startDate = selectedDate;
+		$("#startDateText").text("Start date: "+selectedDate.toDateString());
+		return
+	}
+	endDate = selectedDate;
+	$("#endDateText").text("End date: "+selectedDate.toDateString());
+	
+	diffDays = endDate.diff(startDate, "days");
+	log2Console("Startdate:"+startDate.toDateString()+" EndDate:"+endDate.toDateString());
+	log2Console("Startdate:"+startDate.toLocaleString()+" EndDate:"+endDate.toLocaleString());
+	directDateDiff = endDate.getTime()-startDate.getTime();
+	startDateTime = startDate.getTime();
+	
+	log2Console("Difference between dates:"+diffDays);
+	
+	
+}
+
 
 function changeNumberOfRowsCols(){
 	$('#calendarPlace').datepicker("option",
